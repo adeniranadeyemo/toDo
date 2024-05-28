bcb052afb249ffe4de48fb0c714d93276e1c5e5a
adeyemoadeniran <niranadeniran1@gmail.com>
2024-05-28 13:46:13 +0100

    Update

--- todo.js
+++ todo.js
@@ -39,6 +39,7 @@ function toggleTaskContainer() {
   elementToggle(overlay);
   elementToggle(inputContainer);
   elementToggle(showTaskCon);
+  inputedTodo.focus();
 }
 
 showTaskCon.addEventListener('click', toggleTaskContainer);
@@ -86,13 +87,23 @@ const generateHTML = function (todo) {
           </div>`;
 };
 
+// Persistence from local storage
+function persistence(todos, todoId) {
+  localStorage.setItem('data', JSON.stringify(todos));
+  localStorage.setItem('todoId', todoId.toString());
+
+  renderAll();
+}
+
 // Tasks functionalities start
 let todoId = localStorage.getItem('todoId')
   ? parseInt(localStorage.getItem('todoId'))
   : 0;
-const addTodo = function () {
+
+const addOrUpdateTodo = function () {
   if (inputedTodo.value !== '') {
     const todoIndex = todos.findIndex((todo) => todo.id === currentTask.id);
+    console.log(todoIndex);
 
     const todoObj = {
       id: todoId,
@@ -103,96 +114,70 @@ const addTodo = function () {
 
     if (todoIndex === -1) {
       todos.push(todoObj);
-      reset;
+      todoId++;
+      console.log(todoObj.id);
     } else {
-      todos[todoIndex] = todoObj;
+      todos[todoIndex] = currentTask;
+      currentTask.text = inputedTodo.value;
+      currentTask = {};
     }
-
-    localStorage.setItem('data', JSON.stringify(todos));
   }
-  todoId++;
 
   inputedTodo.value = '';
 
   persistence(todos, todoId);
 };
 
-window.addEventListener('DOMContentLoaded', function () {
+// Straight to favourite
+addToFavourites.addEventListener('click', function () {
+  if (inputedTodo.value !== '') {
+    const todoObj = {
+      id: todoId,
+      text: inputedTodo.value,
+      favorite: true,
+      done: false,
+    };
+
+    todos.push(todoObj);
+    inputedTodo.value = '';
+  }
+  todoId++;
+
   persistence(todos, todoId);
-  // localStorage.clear();
+
+  toggleTaskContainer();
 });
 
-// Edit todo
-function editTodo(index) {
-  currentTask = todos[index];
+// Implementing the addTodo function
+function addToTodoContainer() {
+  if (inputedTodo.value !== '') {
+    addOrUpdateTodo();
 
-  inputedTodo.value = currentTask.text;
+    toDoWelcomeSubText.innerHTML = `Your to-do's.`;
 
+    inputedTodo.value = '';
+  }
   toggleTaskContainer();
 }
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addToTodoContainer();
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  persistence(todos, todoId);
+});
+
+// Reset
 function reset() {
   inputedTodo.value = '';
   currentTask = {};
-  toggleTaskContainer();
+  overlay.classList.remove('active');
 }
 
-function persistence(todos, todoId) {
-  localStorage.setItem('data', JSON.stringify(todos));
-  localStorage.setItem('todoId', todoId.toString());
-
-  renderAll();
-}
-
-function renderAll() {
-  renderTodos();
-  renderFavourites();
-  renderDone();
-}
-
-// Delete a to-do
-function deleteTodo(index) {
-  if (index !== -1) {
-    todos.splice(index, 1);
-  }
-
-  todos.length === 0
-    ? (toDoWelcomeSubText.innerHTML = `Create to-do's`)
-    : `Your to-do's`;
-
-  persistence(todos, todoId);
-}
-
-// Toggle favorite
-function toggleFavorite(id) {
-  const todo = todos.find((todo) => id === todo.id);
-
-  if (todo) {
-    todo.favorite = !todo.favorite;
-    if (!todo.favorite) {
-      const favIndex = favArr.findIndex((todo) => todo.id === id);
-      favArr.splice(favIndex, 1);
-    }
-  }
-
-  persistence(todos, todoId);
-}
-
-// localStorage.clear();
-
-function toggleDone(id) {
-  const todo = todos.find((todo) => id === todo.id);
-
-  if (todo) {
-    todo.done = !todo.done;
-    if (!todo.done) {
-      const doneIndex = doneArr.findIndex((todo) => id === todo.id);
-      doneArr.splice(doneIndex, 1);
-    }
-  }
-
-  persistence(todos, todoId);
-}
+overlay.addEventListener('click', reset);
 
 // Render to do
 const renderTodos = function () {
@@ -218,6 +203,7 @@ const renderFavourites = function () {
   });
 };
 
+// Render done
 const renderDone = function () {
   doneTasks.innerHTML = '';
 
@@ -230,60 +216,78 @@ const renderDone = function () {
   });
 };
 
-// Implementing the addTodo function
-function addToTodoContainer() {
-  if (inputedTodo.value !== '') {
-    addTodo();
-
-    toDoWelcomeSubText.innerHTML = `Your to-do's.`;
-
-    inputedTodo.value = '';
-  }
-  toggleTaskContainer();
+function renderAll() {
+  renderTodos();
+  renderFavourites();
+  renderDone();
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  addToTodoContainer();
-});
-
-// Straight to favourite
-addToFavourites.addEventListener('click', function () {
-  if (inputedTodo.value !== '') {
-    let text = inputedTodo.value;
-
-    const todoObj = {
-      id: todoId,
-      text,
-      favorite: false,
-      done: false,
-    };
-
-    todos.push(todoObj);
-    inputedTodo.value = '';
+// Delete a to-do
+function deleteTodo(index) {
+  if (index !== -1) {
+    todos.splice(index, 1);
   }
-  todoId++;
+
+  todos.length === 0
+    ? (toDoWelcomeSubText.innerHTML = `Create to-do's`)
+    : `Your to-do's`;
+
+  if (todos.length === 0) {
+    todoId = 0;
+  }
 
   persistence(todos, todoId);
+}
+
+// Toggle favorite
+function toggleFavorite(id) {
+  const todo = todos.find((todo) => id === todo.id);
+
+  if (todo) {
+    todo.favorite = !todo.favorite;
+    if (!todo.favorite) {
+      const favIndex = favArr.findIndex((todo) => todo.id === id);
+      favArr.splice(favIndex, 1);
+    }
+  }
+
+  persistence(todos, todoId);
+}
+
+function toggleDone(id) {
+  const todo = todos.find((todo) => id === todo.id);
+
+  if (todo) {
+    todo.done = !todo.done;
+    if (!todo.done) {
+      const doneIndex = doneArr.findIndex((todo) => id === todo.id);
+      doneArr.splice(doneIndex, 1);
+    }
+  }
+
+  persistence(todos, todoId);
+}
+
+// Edit todo
+function editTodo(index) {
+  currentTask = todos[index];
+
+  inputedTodo.value = currentTask.text;
 
   toggleTaskContainer();
-});
 
-// window.addEventListener('DOMContentLoaded', function () {
-//   persistence(todos, todoId);
-//   // localStorage.clear();
-// });
+  inputedTodo.focus();
+}
 
-// Event delegation for delete buttons on each page
-let delIndex;
+// Event delegation for delete and edit buttons on each page
+let taskIndex;
 
 const findIndexFunc = function (e) {
   const paragraphElem = e.target.parentNode.parentNode.querySelector('p');
 
   const parentText = paragraphElem.textContent;
 
-  delIndex = todos.findIndex((todo) => todo.text === parentText);
+  taskIndex = todos.findIndex((todo) => todo.text === parentText);
 };
 
 //
@@ -291,7 +295,7 @@ const del = function (e) {
   if (e.target.classList.contains('delete-todo')) {
     findIndexFunc(e);
 
-    deleteTodo(delIndex);
+    deleteTodo(taskIndex);
   }
 };
 
@@ -299,17 +303,20 @@ const edit = function (e) {
   if (e.target.classList.contains('edit-todo')) {
     findIndexFunc(e);
 
-    editTodo(delIndex);
+    editTodo(taskIndex);
   }
 };
 
-tasks.addEventListener('click', function (e) {
+function setEvents(e) {
   del(e);
   edit(e);
-});
+}
 
-favs.addEventListener('click', del);
-doneTasks.addEventListener('click', del);
+tasks.addEventListener('click', setEvents);
+
+favs.addEventListener('click', setEvents);
+
+doneTasks.addEventListener('click', setEvents);
 
 // Switch pages
 for (let i = 0; i < navigationLinks.length; i++) {
